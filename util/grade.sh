5ae80deb855a20004037a23dc44533782793cb2a
chenqiang1986 <chenqiang19860101@gmail.com>
2025-11-06 08:46:00 -0800

    Update grade.sh

--- util/grade.sh
+++ util/grade.sh
@@ -1,4 +1,28 @@
 ./$1 > $1.out
-diff -q $1.out ../$1.out || echo "Test Case 1 Failed"
-diff -q ../$1.out $1.out || echo "Test Case 2 Failed"
-diff -q $1.out $1.out || echo "Test Case 3 Failed"
+
+total_failure=0
+
+diff --brief $1.out ../$1.out >/dev/null
+comp_value=$?
+
+if [ $comp_value -eq 1 ]
+then
+    total_failure = total_failure+1
+    echo "Test Case 1 Failed"
+else
+    echo "Test Case 1 Passed"
+fi
+
+
+diff --brief $1.out $1.out >/dev/null
+comp_value=$?
+
+if [ $comp_value -eq 1 ]
+then
+    total_failure = total_failure+1
+    echo "Test Case 2 Failed"
+else
+    echo "Test Case 2 Passed"
+fi
+
+echo "$total_failure Failures"
