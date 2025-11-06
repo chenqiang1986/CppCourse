7c9e7f6f6861d081b52f2e075f3b918f88cf6162
chenqiang1986 <chenqiang19860101@gmail.com>
2025-11-06 13:30:16 -0800

    Update grade.sh (#9)
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh

--- util/grade.sh
+++ util/grade.sh
@@ -2,27 +2,29 @@
 
 total_failure=0
 
-diff --brief $1.out ../$1.out >/dev/null
+diff --brief ../$1.out ../$1.out >/dev/null
 comp_value=$?
 
 if [ $comp_value -eq 1 ]
 then
-    total_failure = $((total_failure+1))
+    echo "Add into Total"
+    total_failure=$((total_failure+1))
     echo "Test Case 1 Failed"
 else
     echo "Test Case 1 Passed"
 fi
-
 
 diff --brief $1.out $1.out >/dev/null
 comp_value=$?
 
 if [ $comp_value -eq 1 ]
 then
-    total_failure = total_failure+1
+    echo "Add into Total"
+    total_failure=$((total_failure+1))
     echo "Test Case 2 Failed"
 else
     echo "Test Case 2 Passed"
 fi
 
 echo "$total_failure Failures"
+exit $total_failure
