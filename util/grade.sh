bdf8bd48520447de3b20be38878ea25f09d9bd37
chenqiang1986 <chenqiang19860101@gmail.com>
2025-11-06 08:40:27 -0800

    Update grade.sh

--- util/grade.sh
+++ util/grade.sh
@@ -1,3 +1,4 @@
 ./$1 > $1.out
-diff $1.out ../$1.out
-diff ../$1.out $1.out
+diff -q $1.out ../$1.out || echo "Test Case 1 Failed"
+diff -q ../$1.out $1.out || echo "Test Case 2 Failed"
+diff -q $1.out $1.out || echo "Test Case 3 Failed"
