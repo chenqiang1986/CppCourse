a664e130e543cd6607371479d08dd767f2b6aa48
chenqiang1986 <chenqiang19860101@gmail.com>
2025-11-06 17:05:54 -0800

    Update grade.sh

--- util/grade.sh
+++ util/grade.sh
@@ -9,20 +9,31 @@ for val in "${arr[@]}"; do
        non_ext_base_name=${base_name%.*}
        
        echo " - Run Target: build/$non_ext_base_name"
-       echo "   - Run Test Case 1"
-       build/$non_ext_base_name < answers/$non_ext_base_name/1.in > build/$non_ext_base_name.out
-       
-       diff -q answers/$non_ext_base_name/1.out build/$non_ext_base_name.out >/dev/null
-       comp_value=$?
-       if [ $comp_value -eq 1 ]
-       then
-           total_failure=$((total_failure+1))
-           echo "   - Test Case 1 Failed, See Diff Below"
-           diff answers/$non_ext_base_name/1.out build/$non_ext_base_name.out
-       else
-           echo "   - Test Case 1 Passed"
-       fi
-       echo " "
+
+       i=1
+       while :
+       do
+          if [ $i -eq 3 ]; then
+              echo "This is the end of the loop"
+              break
+          fi
+          
+          echo "   - Run Test Case $i"
+          build/$non_ext_base_name < answers/$non_ext_base_name/$i.in > build/$non_ext_base_name.out
+          
+          diff -q answers/$non_ext_base_name/$i.out build/$non_ext_base_name.out >/dev/null
+          comp_value=$?
+          if [ $comp_value -eq 1 ]
+          then
+              total_failure=$((total_failure+1))
+              echo "   - Test Case $i Failed, See Diff Below"
+              diff answers/$non_ext_base_name/$i.out build/$non_ext_base_name.out
+          else
+              echo "   - Test Case $i Passed"
+          fi
+          echo " "
+          ((i++))
+       done
    fi
 done
 
