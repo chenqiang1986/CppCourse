d738e63f77d00a4f4c5e509eef0c408ce21780da
chenqiang1986 <chenqiang19860101@gmail.com>
2025-11-06 17:00:21 -0800

    Update grade.sh

--- util/grade.sh
+++ util/grade.sh
@@ -3,7 +3,6 @@ input=$1
 read -ra arr <<< "$input"
 
 for val in "${arr[@]}"; do
-   printf "file: $val\n"
    if [[ $val == *.cpp ]]
    then
        base_name=$(basename $val)
@@ -23,6 +22,7 @@ for val in "${arr[@]}"; do
        else
            echo "   - Test Case 1 Passed"
        fi
+       echo " "
    fi
 done
 
