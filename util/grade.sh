3302f364bac43550aca7bb4c59d60bfaefd07c2b
chenqiang1986 <chenqiang19860101@gmail.com>
2025-11-06 17:10:15 -0800

    Update grade.sh (#12)
    * Update grade.sh
    
    * Update First.cpp
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh
    
    * Update grade.sh

--- util/grade.sh
+++ util/grade.sh
@@ -3,25 +3,37 @@ input=$1
 read -ra arr <<< "$input"
 
 for val in "${arr[@]}"; do
-   printf "file: $val\n"
    if [[ $val == *.cpp ]]
    then
        base_name=$(basename $val)
        non_ext_base_name=${base_name%.*}
        
        echo " - Run Target: build/$non_ext_base_name"
-       build/$non_ext_base_name > build/$non_ext_base_name.out
 
-       diff --brief $non_ext_base_name.out build/$non_ext_base_name.out >/dev/null
-       comp_value=$?
-       if [ $comp_value -eq 1 ]
-       then
-           echo "Add into Total"
-           total_failure=$((total_failure+1))
-           echo "Test Case 1 Failed"
-       else
-           echo "Test Case 1 Passed"
-       fi
+       i=1
+       while :
+       do
+          if [ ! -f answers/$non_ext_base_name/$i.in ]; then
+              echo "This is the end of the loop"
+              break
+          fi
+          
+          echo "   - Run Test Case $i"
+          build/$non_ext_base_name < answers/$non_ext_base_name/$i.in > build/$non_ext_base_name.out
+          
+          diff -q answers/$non_ext_base_name/$i.out build/$non_ext_base_name.out >/dev/null
+          comp_value=$?
+          if [ $comp_value -eq 1 ]
+          then
+              total_failure=$((total_failure+1))
+              echo "   - Test Case $i Failed, See Diff Below"
+              diff answers/$non_ext_base_name/$i.out build/$non_ext_base_name.out
+          else
+              echo "   - Test Case $i Passed"
+          fi
+          echo " "
+          ((i++))
+       done
    fi
 done
 
