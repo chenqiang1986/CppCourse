e6882454d4574e0e736219f677141ff38da176d6
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2025-11-09 19:36:38 -0800

    cow_stalls.cpp

---
+++ Henry/cow_stalls.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+std::vector<int> find_count_cows_under(const std::vector<int>& stalls, const std::vector<int>& cows){
+    std::vector<int> count_cows_under;
+    for(int i = 0; i < stalls.size(); i++){
+        int count = 0;
+        for(int j = 0; j < cows.size(); j++){
+            if(stalls[i] >= cows[j]){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        count_cows_under.push_back[count];
+    }
+    return count_cows_under;
+}
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::vector<int> cows;
+    std::vector<int> stalls;
+    for(int i = 0; i < n; i++){
+        int cow;
+        std::cin >> cow;
+        cows.push_back(cow);
+    }
+    for(int i = 0; i < n; i++){
+        int stall;
+        std::cin >> stall;
+        stalls.push_back(stall);
+    }
+    std::sort(cows.begin(), cows.end());
+    std::sort(stalls.begin(), stalls.end());
+    std::vector<int> count_cows_under = find_count_cows_under(stalls, cows);
+    long long num_ways = 1;
+    bool broken_out = false;
+    for(int i = 0; i < count_cows_under.size(); i++){
+        num_ways *= count_cows_under[i] - i;
+        if(num_ways <= 0){
+            broken_out = true;
+            break;
+        }
+    }
+    if(broken_out){
+        std::cout << 0 << std::endl;
+    }
+    else{
+        std::cout << num_ways << std::endl;
+    }
+    return 0;
+}
