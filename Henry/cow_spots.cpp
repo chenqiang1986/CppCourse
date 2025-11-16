3dd27537e1ed48d4ea8026705e8546ef934db1bb
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2025-11-16 14:05:48 -0800

    Update cow_spots.cpp

--- Henry/cow_spots.cpp
+++ Henry/cow_spots.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
 #include <set>
@@ -66,8 +67,8 @@ void find_cc(const std::vector<std::string>& cow, std::map<Coord, int>& point_to
 
 int find_shortest_dist(CCPair& cc_pair, std::map<int, std::set<Coord>>& cc_to_points){
     int min_dist = 1000000;
-    for(auto point1 : cc_to_points[cc_pair.cc1]){
-        for(auto point2 : cc_to_points[cc_pair.cc2]){
+    for(auto& point1 : cc_to_points[cc_pair.cc1]){
+        for(auto& point2 : cc_to_points[cc_pair.cc2]){
             int manhattan_dist = std::max(point1.row, point2.row) - std::min(point1.row, point2.row) + std::max(point1.col, point2.col) - std::min(point1.col, point2.col);
             int blocks_went_over = manhattan_dist - 1;
             min_dist = std::min(blocks_went_over, min_dist);
@@ -76,12 +77,12 @@ int find_shortest_dist(CCPair& cc_pair, std::map<int, std::set<Coord>>& cc_to_po
     return min_dist;
 }
 
-int find_shortest_total(std::map<int, std::set<Coord>>& cc_to_points){
+int find_shortest_total_sperate(std::map<int, std::set<Coord>>& cc_to_points){
     int total = 0;
     int added_in = 0;
     std::map<CCPair, int> cc_pair_to_shortest_dist;
-    for(auto [ccid1, points1] : cc_to_points){
-        for(auto [ccid2, points2] : cc_to_points){
+    for(auto& [ccid1, points1] : cc_to_points){
+        for(auto& [ccid2, points2] : cc_to_points){
             CCPair cc_pair = CCPair{.cc1 = ccid1, .cc2 = ccid2};
             cc_pair.normalize();
             if(ccid1 == ccid2 || cc_pair_to_shortest_dist.count(cc_pair) != 0){
@@ -103,6 +104,30 @@ int find_shortest_total(std::map<int, std::set<Coord>>& cc_to_points){
     return total;
 }
 
+int find_shortest_dist_point_cc(std::set<Coord>& points, Coord& point1){
+    int min_dist = 10000000;
+    for(auto& point2 : points){
+        int manhattan_dist = std::abs(point1.row - point2.row) + std::abs(point1.col - point2.col);
+        min_dist = std::min(manhattan_dist, min_dist);
+    }
+    return min_dist;
+}
+
+int find_shortest_total_combined(std::map<int, std::set<Coord>>& cc_to_points, int n, int m){
+    int shortest_dist = 10000000;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            Coord point = Coord{.row = i, .col = j};
+            int dist = 1;
+            for(auto& [ccid, points] : cc_to_points){
+                dist += find_shortest_dist_point_cc(points, point) - 1;
+            }
+            shortest_dist = std::min(shortest_dist, dist);
+        }
+    }
+    return shortest_dist;
+}
+
 int main(){
     int n, m;
     std::cin >> n >> m;
@@ -115,6 +140,6 @@ int main(){
     std::map<Coord, int> point_to_cc;
     std::map<int, std::set<Coord>> cc_to_points;
     find_cc(cow, point_to_cc, cc_to_points);
-    std::cout << find_shortest_total(cc_to_points) << std::endl;
+    std::cout << std::min(find_shortest_total_sperate(cc_to_points), find_shortest_total_combined(cc_to_points, n, m)) << std::endl;
     return 0;
 }
