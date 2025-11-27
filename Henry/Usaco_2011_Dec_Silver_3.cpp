7a70518cc61245ae624f57b3ebabed8a04808c99
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2025-11-27 15:04:34 -0800

    Update Usaco_2011_Dec_Silver_3.cpp

--- Henry/Usaco_2011_Dec_Silver_3.cpp
+++ Henry/Usaco_2011_Dec_Silver_3.cpp
@@ -4,15 +4,28 @@
 #include <set>
 #include <vector>
 
-int find_min_cost(const std::vector<int>& cow_positions, std::map<int, int> size_to_cost, int starting_index){
+void optimize(std::map<int, int>& size_to_cost){
+    int min = 2100000000;
+    for(int i = size_to_cost.size(); i > 0; i--){
+        min = std::min(min, size_to_cost[i]);
+        size_to_cost[i] = min;
+    }
+}
+
+int find_min_cost(const std::vector<int>& cow_positions, std::map<int, int> size_to_cost, int starting_index, std::map<int, int>& cache){
+    if(cache.count(starting_index) != 0){
+        return cache[starting_index];
+    }
     if(starting_index >= cow_positions.size()){
+        cache[starting_index] = 0;
         return 0;
     }
     int min_cost = size_to_cost[size_to_cost.size()];
     for(int i = starting_index; i < cow_positions.size(); i++){
         int width = cow_positions[i] - cow_positions[starting_index] + 1;
-        min_cost = std::min(min_cost, size_to_cost[width] + find_min_cost(cow_positions, size_to_cost, i + 1));
+        min_cost = std::min(min_cost, size_to_cost[width] + find_min_cost(cow_positions, size_to_cost, i + 1, cache));
     }
+    cache[starting_index] = min_cost;
     return min_cost;
 }
 
@@ -32,6 +45,8 @@ int main(){
         std::cin >> cost;
         size_to_cost[i] = cost;
     }
-    std::cout << find_min_cost(cow_positions, size_to_cost, 0) << std::endl;
+    optimize(size_to_cost);
+    std::map<int, int> cache;
+    std::cout << find_min_cost(cow_positions, size_to_cost, 0, cache) << std::endl;
     return 0;
 }
