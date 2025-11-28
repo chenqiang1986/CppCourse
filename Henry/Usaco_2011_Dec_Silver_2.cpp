c2bdae00ce792f009db3a85a121ebc30f1bbe3d4
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2025-11-27 17:02:53 -0800

    Update Usaco_2011_Dec_Silver_2.cpp

--- Henry/Usaco_2011_Dec_Silver_2.cpp
+++ Henry/Usaco_2011_Dec_Silver_2.cpp
@@ -1,74 +1,91 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <set>
+#include <optional>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
-struct Segment{
-    int endpoint1;
-    int endpoint2;
-    int length;
-
-    bool operator<(const Segment& other)const{
-        return this->endpoint1 < other.endpoint1 || (this->endpoint1 == other.endpoint1 && this->endpoint2 < other.endpoint2);
-    }
-    bool operator==(const Segment& other)const{
-        return (this->endpoint1 == other.endpoint1 && this->endpoint2 == other.endpoint2);
-    }
-};
-
-long find_best(std::map<int, std::set<Segment>>& start_to_segment, int start, int end, std::set<int>& visited){
-    if(start == end){
-        return 0;
-    }
-    long best = 1000000000000000000;
-    for(auto segment : start_to_segment[start]){
-        if(visited.count(segment.endpoint2) == 0){
-            visited.insert(segment.endpoint2);
-            best = std::min(best, find_best(start_to_segment, segment.endpoint2, end, visited) + segment.length);
-            visited.erase(segment.endpoint2);
+std::optional<int> find_least_dist_to_start(std::unordered_map<int, int>& dist_to_start, std::unordered_set<int>& visited){
+    int least_dist = 2100000000;
+    std::optional<int> least_dist_field = std::nullopt;
+    for(auto [field, dist] : dist_to_start){
+        if(visited.count(field) == 0){
+            if(dist < least_dist){
+                least_dist = dist;
+                least_dist_field = field;
+            }
         }
     }
-    return best;
+    return least_dist_field;
 }
 
-long find_best_double(std::map<int, std::set<Segment>>& start_to_segment, int start, int end, std::set<int>& visited, Segment& doubled_segment1, Segment& doubled_segment2){
-    if(start == end){
-        return 0;
-    }
-    long best = 1000000000000000000;
-    for(auto segment : start_to_segment[start]){
-        if(visited.count(segment.endpoint2) == 0){
-            visited.insert(segment.endpoint2);
-            best = std::min(best, find_best_double(start_to_segment, segment.endpoint2, end, visited, doubled_segment1, doubled_segment2) + (segment == doubled_segment1 || segment == doubled_segment2 ? segment.length * 2 : segment.length));
-            visited.erase(segment.endpoint2);
+void update_neighbors(int seed, std::unordered_map<int, std::unordered_map<int, int>>& farm, std::unordered_map<int, int>& dist_to_start, std::unordered_map<int, int>& from_node){
+    for(auto [neighbor, dist] : farm[seed]){
+        int through_seed = dist + dist_to_start[seed];
+        if(dist_to_start.count(neighbor) == 0 || through_seed < dist_to_start[neighbor]){
+            dist_to_start[neighbor] = through_seed;
+            from_node[neighbor] = seed;
         }
     }
-    return best;
+}
+
+void find_min_path(std::unordered_map<int, std::unordered_map<int, int>>& farm, int start, int end, std::optional<int>& min_dist, std::vector<int>& min_path){
+    std::unordered_map<int, int> dist_to_start;
+    dist_to_start[start] = 0;
+    std::unordered_map<int, int> from_node;
+    std::unordered_set<int> visited;
+    while(true){
+        std::optional<int> seed = find_least_dist_to_start(dist_to_start, visited);
+        if(! seed.has_value()){
+            break;
+        }
+        visited.insert(*seed);
+        update_neighbors(*seed, farm, dist_to_start, from_node);
+    }
+    if(dist_to_start.count(end) != 0){
+        min_dist = dist_to_start[end];
+        int field = end;
+        while(field != start){
+            min_path.push_back(field);
+            field = from_node[field];
+        }
+        min_path.push_back(start);
+    }
+    else{
+        min_dist = std::nullopt;
+    }
+}
+
+int find_max_raise(std::unordered_map<int, std::unordered_map<int, int>>& farm, int start, int end, int min_dist, std::vector<int>& min_path){
+    int max_min_dist = min_dist;
+    for(int i = 1; i < min_path.size(); i++){
+        farm[min_path[i - 1]][min_path[i]] *= 2;
+        farm[min_path[i]][min_path[i - 1]] *= 2;
+        std::optional<int> new_min_dist;
+        std::vector<int> new_min_path;
+        find_min_path(farm, start, end, new_min_dist, new_min_path);
+        max_min_dist = std::max(max_min_dist, *new_min_dist);
+        farm[min_path[i - 1]][min_path[i]] /= 2;
+        farm[min_path[i]][min_path[i - 1]] /= 2;
+    }
+    return max_min_dist - min_dist;
 }
 
 int main(){
     int n, m;
     std::cin >> n >> m;
-    std::map<int, std::set<Segment>> start_to_segment;
+    std::unordered_map<int, std::unordered_map<int, int>> farm;
     for(int i = 0; i < m; i++){
         int start, end, length;
         std::cin >> start >> end >> length;
-        start_to_segment[start].insert(Segment{.endpoint1 = start, .endpoint2 = end, .length = length});
-        start_to_segment[end].insert(Segment{.endpoint1 = end, .endpoint2 = start, .length = length});
+        farm[start][end] = length;
+        farm[end][start] = length;
     }
-    std::set<int> visited = {1};
-    long shortest_path = find_best(start_to_segment, 1, n, visited);
-    long max_shortest_path_after_roadblock = 0;
-    for(auto [start, segments] : start_to_segment){
-        for(auto segment : segments){
-            for(auto other_segment : start_to_segment[segment.endpoint2]){
-                if(other_segment.endpoint2 == start){
-                    max_shortest_path_after_roadblock = std::max(find_best_double(start_to_segment, 1, n, visited, segment, other_segment), max_shortest_path_after_roadblock);
-                }
-            }
-        }
+    std::optional<int> min_dist;
+    std::vector<int> min_path;
+    find_min_path(farm, 1, n, min_dist, min_path);
+    if(min_dist.has_value()){
+        std::cout << find_max_raise(farm, 1, n, *min_dist, min_path) << std::endl;
     }
-    std::cout << max_shortest_path_after_roadblock - shortest_path << std::endl;
     return 0;
 }
