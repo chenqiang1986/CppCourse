e1ddd68baf151ad47c56faee6067655a1cde34a2
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2026-01-02 14:38:54 -0800

    Update Usaco_2012_Mar_Silver_3.cpp

--- Henry/Usaco_2012_Mar_Silver_3.cpp
+++ Henry/Usaco_2012_Mar_Silver_3.cpp
@@ -1,35 +1,51 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
-#include <set>
+#include <vector>
 
-struct StartEnd{
-    int start;
-    int end;
+struct Earth{
+    int quantity;
+    int pos;
 };
+
+std::vector<Earth> find_earths(std::map<int, int>& plot_to_diff){
+    std::vector<Earth> earths;
+    for(auto& [plot, diff] : plot_to_diff){
+        for(int i = 0; i < std::abs(diff); i++){
+            earths.push_back(Earth{.quantity = (diff > 0 ? 1 : -1), .pos = plot});
+        }
+    }
+    return earths;
+}
+
+int cost(std::vector<Earth>& earths, int index, int extra_earth, int add, int remove, int move){
+    if(index == earths.size() - 1){
+        int earth_amount = extra_earth + earths[index].quantity;
+        return std::abs(earth_amount) * (earth_amount > 0 ? add : remove);
+    }
+    int sell_or_buy_cost = (earths[index].quantity == 1 ? add : remove);
+    int moving_base = (earths[index + 1].pos - earths[index].pos) * move;
+    int output = std::min(
+        sell_or_buy_cost + cost(earths, index + 1, extra_earth, add, remove, move) + std::abs(extra_earth) * moving_base,
+        cost(earths, index + 1, extra_earth + earths[index].quantity, add, remove, move) + std::abs(extra_earth + earths[index].quantity) * moving_base
+    );
+    return output;
+}
+
+int find_min_cost(std::map<int, int>& plot_to_diff, int add, int remove, int move){
+    std::vector<Earth> earths = find_earths(plot_to_diff);
+    return cost(earths, 0, 0, add, remove, move);
+}
 
 int main(){
     int n, x, y, z;
     std::cin >> n >> x >> y >> z;
-    std::map<int, StartEnd> plot_to_dirt_start_end; 
+    std::map<int, int> plot_to_diff; 
     for(int i = 0; i < n; i++){
         int start, end;
         std::cin >> start >> end;
-        plot_to_dirt_start_end[i] = StartEnd{.start = start, .end = end};
+        plot_to_diff[i] = end - start;
     }
-    if(z > x + y){
-        int cost = 0;
-        for(int i = 0; i < n; i++){
-            if(plot_to_dirt_start_end[i].start > plot_to_dirt_start_end[i].end){
-                cost += y * (plot_to_dirt_start_end[i].start - plot_to_dirt_start_end[i].end);
-            }
-            else{
-                cost += x * (plot_to_dirt_start_end[i].end - plot_to_dirt_start_end[i].start);
-            }
-        }
-        std::cout << cost << std::endl;
-    }
-    else{
-        std::cout << 210 << std::endl;
-    }
+    std::cout << find_min_cost(plot_to_diff, x, y, z) << std::endl;
     return 0;
 }
