b91ae5294f94838c3ddddcd1fbcdd27785c3fcbb
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2026-01-04 11:27:44 -0800

    Update Usaco_2012_Open_Silver_3.cpp

--- Henry/Usaco_2012_Open_Silver_3.cpp
+++ Henry/Usaco_2012_Open_Silver_3.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <map>
 
-long find_intersection_count(std::map<int, int>& speeds, int laps){
-    std::map<int, int>::iterator it = speeds.end();
-    it--;
-    int max_speed = it->first;
+long find_intersection_count(std::map<long, long>& speeds, long laps){
+    std::map<long, long>::iterator it = std::next(speeds.end(), -1);
+    long max_speed = it->first;
     long intersections = 0;
     for(auto& [speed_one, count_one] : speeds){
         for(auto& [speed_two, count_two] : speeds){
             if(speed_one < speed_two){
-                long one_to_one = (laps * (speed_two - speed_one) / max_speed);
+                long one_to_one = ((laps * (speed_two - speed_one)) / max_speed);
                 intersections += one_to_one * count_one * count_two;
             }
         }
@@ -18,11 +17,11 @@ long find_intersection_count(std::map<int, int>& speeds, int laps){
 }
 
 int main(){
-    int n, l, c;
+    long n, l, c;
     std::cin >> n >> l >> c;
-    std::map<int, int> speeds;
-    for(int i = 0; i < n; i++){
-        int speed;
+    std::map<long, long> speeds;
+    for(long i = 0; i < n; i++){
+        long speed;
         std::cin >> speed;
         speeds[speed]++;
     }
