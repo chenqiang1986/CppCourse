cea01e154c759c9b1260c873b0b8c269a1dbe621
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2026-01-04 15:59:04 -0800

    Update Usaco_2012_Open_Silver_1.cpp

--- Henry/Usaco_2012_Open_Silver_1.cpp
+++ Henry/Usaco_2012_Open_Silver_1.cpp
@@ -45,38 +45,38 @@ struct Coord{
 
 struct Block{
     std::set<Coord> components;
-    int top;
-    int bottom;
-    int left;
-    int right;
+    int min_col;
+    int max_col;
+    int min_row;
+    int max_row;
 
     void find_bounding_box(){
-        top = components.begin()->col;
-        bottom = components.begin()->col;
-        left = components.begin()->row;
-        right = components.begin()->row;
+        min_col = components.begin()->col;
+        max_col = components.begin()->col;
+        min_row = components.begin()->row;
+        max_row = components.begin()->row;
         for(auto& coord : components){
-            top = std::min(top, coord.col);
-            bottom = std::max(bottom, coord.col);
-            left = std::min(left, coord.row);
-            right = std::max(right, coord.row);
+            min_col = std::min(min_col, coord.col);
+            max_col = std::max(max_col, coord.col);
+            min_row = std::min(min_row, coord.row);
+            max_row = std::max(max_row, coord.row);
         }
     }
 };
 
 bool is_seperated(const Shifts& first_shifts, const Block& first_block, const Shifts& second_shifts, const Block& second_block){
-    int first_top = first_block.top + first_shifts.col_shift;
-    int first_bottom = first_block.bottom + first_shifts.col_shift;
-    int first_left = first_block.left + first_shifts.row_shift;
-    int first_right = first_block.right + first_shifts.row_shift;
-    int second_top = second_block.top + second_shifts.col_shift;
-    int second_bottom = second_block.bottom + second_shifts.col_shift;
-    int second_left = second_block.left + second_shifts.row_shift;
-    int second_right = second_block.right + second_shifts.row_shift;
-    if(first_top > second_bottom || second_top > first_bottom){
+    int first_min_col = first_block.min_col + first_shifts.col_shift;
+    int first_max_col = first_block.max_col + first_shifts.col_shift;
+    int first_min_row = first_block.min_row + first_shifts.row_shift;
+    int first_max_row = first_block.max_row + first_shifts.row_shift;
+    int second_min_col = second_block.min_col + second_shifts.col_shift;
+    int second_max_col = second_block.max_col + second_shifts.col_shift;
+    int second_min_row = second_block.min_row + second_shifts.row_shift;
+    int second_max_row = second_block.max_row + second_shifts.row_shift;
+    if(first_min_col > second_max_col || second_min_col > first_max_col){
         return true;
     }
-    if(first_left > second_right || second_left > first_right){
+    if(first_min_row > second_max_row || second_min_row > first_max_row){
         return true;
     }
     return false;
@@ -190,186 +190,3 @@ int main(){
     std::cout << min_moves(blocks) << std::endl;
     return 0;
 }
-
-
-
-/*
-#include <algorithm>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
-
-struct Coord{
-    int row;
-    int col;
-
-    bool operator<(const Coord& other)const{
-        return this->row < other.row || (this->row == other.row && this->col < other.col);
-    }
-};
-
-struct Shifts{
-    int row_shift;
-    int col_shift;
-
-    bool operator<(const Shifts& other)const{
-        return this->row_shift < other.row_shift || (this->row_shift == other.row_shift && this->col_shift < other.col_shift);
-    }
-};
-
-struct Block{
-    std::set<Coord> components;
-    int top;
-    int bottom;
-    int left;
-    int right;
-
-    void find_bounding_box(){
-        top = components.begin()->col;
-        bottom = components.begin()->col;
-        left = components.begin()->row;
-        right = components.begin()->row;
-        for(auto& coord : components){
-            top = std::min(top, coord.col);
-            bottom = std::max(bottom, coord.col);
-            left = std::min(left, coord.row);
-            right = std::max(right, coord.row);
-        }
-    }
-};
-
-bool is_seperated(const Shifts& first_shifts, const Block& first_block, const Shifts& second_shifts, const Block& second_block){
-    int first_top = first_block.top + first_shifts.col_shift;
-    int first_bottom = first_block.bottom + first_shifts.col_shift;
-    int first_left = first_block.left + first_shifts.row_shift;
-    int first_right = first_block.right + first_shifts.row_shift;
-    int second_top = second_block.top + second_shifts.col_shift;
-    int second_bottom = second_block.bottom + second_shifts.col_shift;
-    int second_left = second_block.left + second_shifts.row_shift;
-    int second_right = second_block.right + second_shifts.row_shift;
-    if(first_top > second_bottom || second_top > first_bottom){
-        return true;
-    }
-    if(first_left > second_right || second_left > first_right){
-        return true;
-    }
-    return false;
-}
-
-bool is_seperated(const std::vector<Shifts>& curr_shifts, const std::vector<Block>& blocks){
-    for(int i = 0; i < 3; i++){
-        for(int j = i + 1; j < 3; j++){
-            if(! is_seperated(curr_shifts[i], blocks[i], curr_shifts[j], blocks[j])){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-std::set<Shifts> find_possible_moves(Block& moving_block, Shifts& moving_block_shifts, Block& other1, Shifts& other1_shifts, Block& other2, Shifts& other2_shifts){
-    std::set<Shifts> possible_moves;
-    possible_moves.insert(Shifts{.row_shift = 0, .col_shift = -1});
-    possible_moves.insert(Shifts{.row_shift = 0, .col_shift = 1});
-    possible_moves.insert(Shifts{.row_shift = -1, .col_shift = 0});
-    possible_moves.insert(Shifts{.row_shift = 1, .col_shift = 0});
-    for(auto& component : moving_block.components){
-        Coord other1_up = Coord{.row = component.row + moving_block_shifts.row_shift - other1_shifts.row_shift, .col = component.col + moving_block_shifts.col_shift - other1_shifts.col_shift - 1};
-        Coord other1_down = Coord{.row = component.row + moving_block_shifts.row_shift - other1_shifts.row_shift, .col = component.col + moving_block_shifts.col_shift - other1_shifts.col_shift + 1};
-        Coord other1_left = Coord{.row = component.row + moving_block_shifts.row_shift - other1_shifts.row_shift - 1, .col = component.col + moving_block_shifts.col_shift - other1_shifts.col_shift};
-        Coord other1_right = Coord{.row = component.row + moving_block_shifts.row_shift - other1_shifts.row_shift + 1, .col = component.col + moving_block_shifts.col_shift - other1_shifts.col_shift};
-        Coord other2_up = Coord{.row = component.row + moving_block_shifts.row_shift - other2_shifts.row_shift, .col = component.col + moving_block_shifts.col_shift - other2_shifts.col_shift - 1};
-        Coord other2_down = Coord{.row = component.row + moving_block_shifts.row_shift - other2_shifts.row_shift, .col = component.col + moving_block_shifts.col_shift - other2_shifts.col_shift + 1};
-        Coord other2_left = Coord{.row = component.row + moving_block_shifts.row_shift - other2_shifts.row_shift - 1, .col = component.col + moving_block_shifts.col_shift - other2_shifts.col_shift};
-        Coord other2_right = Coord{.row = component.row + moving_block_shifts.row_shift - other2_shifts.row_shift + 1, .col = component.col + moving_block_shifts.col_shift - other2_shifts.col_shift};
-        if(other1.components.count(other1_up) != 0 || other2.components.count(other2_up) != 0){
-            possible_moves.erase(Shifts{.row_shift = 0, .col_shift = -1});
-        }
-        if(other1.components.count(other1_down) != 0 || other2.components.count(other2_down) != 0){
-            possible_moves.erase(Shifts{.row_shift = 0, .col_shift = 1});
-        }
-        if(other1.components.count(other1_left) != 0 || other2.components.count(other2_left) != 0){
-            possible_moves.erase(Shifts{.row_shift = -1, .col_shift = 0});
-        }
-        if(other1.components.count(other1_right) != 0 || other2.components.count(other2_right) != 0){
-            possible_moves.erase(Shifts{.row_shift = 1, .col_shift = 0});
-        }
-    }
-    return possible_moves;
-}
-
-void find_all_possible_moves(std::deque<std::vector<Shifts>>& bfs, std::map<std::vector<Shifts>, int>& shifts_to_min_moves, std::vector<Shifts>& curr_shifts, Block& block1, Block& block2, Block& block3){
-    std::set<Shifts> block1_moves = find_possible_moves(block1, curr_shifts[0], block2, curr_shifts[1], block3, curr_shifts[2]);
-    std::set<Shifts> block2_moves = find_possible_moves(block2, curr_shifts[1], block1, curr_shifts[0], block3, curr_shifts[2]);
-    std::set<Shifts> block3_moves = find_possible_moves(block3, curr_shifts[2], block1, curr_shifts[0], block2, curr_shifts[1]);
-    for(auto& move : block1_moves){
-        std::vector<Shifts> new_shifts = curr_shifts;
-        new_shifts[0].row_shift += move.row_shift;
-        new_shifts[0].col_shift += move.col_shift;
-        if(shifts_to_min_moves.count(new_shifts) == 0 && (new_shifts[0].row_shift < 10 && new_shifts[0].row_shift > -10) && (new_shifts[0].col_shift < 10 && new_shifts[0].col_shift > -10)){
-            bfs.push_back(new_shifts);
-            shifts_to_min_moves[new_shifts] = shifts_to_min_moves[curr_shifts] + 1;
-        }
-    }
-    for(auto& move : block2_moves){
-        std::vector<Shifts> new_shifts = curr_shifts;
-        new_shifts[1].row_shift += move.row_shift;
-        new_shifts[1].col_shift += move.col_shift;
-        if(shifts_to_min_moves.count(new_shifts) == 0 && (new_shifts[1].row_shift < 10 && new_shifts[1].row_shift > -10) && (new_shifts[1].col_shift < 10 && new_shifts[1].col_shift > -10)){
-            bfs.push_back(new_shifts);
-            shifts_to_min_moves[new_shifts] = shifts_to_min_moves[curr_shifts] + 1;
-        }
-    }
-    for(auto& move : block3_moves){
-        std::vector<Shifts> new_shifts = curr_shifts;
-        new_shifts[2].row_shift += move.row_shift;
-        new_shifts[2].col_shift += move.col_shift;
-        if(shifts_to_min_moves.count(new_shifts) == 0 && (new_shifts[2].row_shift < 10 && new_shifts[2].row_shift > -10) && (new_shifts[2].col_shift < 10 && new_shifts[2].col_shift > -10)){
-            bfs.push_back(new_shifts);
-            shifts_to_min_moves[new_shifts] = shifts_to_min_moves[curr_shifts] + 1;
-        }
-    }
-}
-
-int min_moves(std::vector<Block> blocks){
-    std::vector<Shifts> block_shifts;
-    block_shifts.push_back(Shifts{.row_shift = 0, .col_shift = 0});
-    block_shifts.push_back(Shifts{.row_shift = 0, .col_shift = 0});
-    block_shifts.push_back(Shifts{.row_shift = 0, .col_shift = 0});
-    std::map<std::vector<Shifts>, int> shifts_to_min_moves;
-    std::deque<std::vector<Shifts>> bfs;
-    bfs.push_back(block_shifts);
-    shifts_to_min_moves[block_shifts] = 0;
-    while(! bfs.empty()){
-        std::vector<Shifts> curr_shifts = bfs.front();
-        if(is_seperated(curr_shifts, blocks)){
-            return shifts_to_min_moves[curr_shifts];
-        }
-        bfs.pop_front();
-        find_all_possible_moves(bfs, shifts_to_min_moves, curr_shifts, blocks[0], blocks[1], blocks[2]);
-    }
-    return -1;
-}
-
-int main(){
-    std::vector<int> block_sizes;
-    for(int i = 0; i < 3; i++){
-        int block_size;
-        std::cin >> block_size;
-        block_sizes.push_back(block_size);
-    }
-    std::vector<Block> blocks (3, Block{});
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < block_sizes[i]; j++){
-            Coord component;
-            std::cin >> component.row >> component.col;
-            blocks[i].components.insert(component);
-        }
-        blocks[i].find_bounding_box();
-    }
-    std::cout << min_moves(blocks) << std::endl;
-    return 0;
-}
-*/
