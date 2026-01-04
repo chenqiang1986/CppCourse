4d1d46299db9108fe110b9f054129ab16ab173a2
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2026-01-04 11:46:42 -0800

    Update Usaco_2012_Open_Silver_3.cpp

--- Henry/Usaco_2012_Open_Silver_3.cpp
+++ Henry/Usaco_2012_Open_Silver_3.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <unordered_map>
+#include <map>
 
-long find_intersection_count(std::unordered_map<long, long>& speeds, long laps){
-    std::unordered_map<long, long>::iterator it = std::next(speeds.end(), -1);
+long find_intersection_count(std::map<long, long>& speeds, long laps){
+    std::map<long, long>::iterator it = std::next(speeds.end(), -1);
     long max_speed = it->first;
     long intersections = 0;
-    for(std::unordered_map<long, long>::iterator i = speeds.begin(); i != speeds.end(); i++){
-        for(std::unordered_map<long, long>::iterator j = i; j != speeds.end(); j++){
+    for(std::map<long, long>::iterator i = speeds.begin(); i != speeds.end(); i++){
+        for(std::map<long, long>::iterator j = i; j != speeds.end(); j++){
             if(i != j){
                 long one_to_one = ((laps * (j->first - i->first)) / max_speed);
                 intersections += one_to_one * j->second * i->second;
@@ -19,7 +19,7 @@ long find_intersection_count(std::unordered_map<long, long>& speeds, long laps){
 int main(){
     long n, l, c;
     std::cin >> n >> l >> c;
-    std::unordered_map<long, long> speeds;
+    std::map<long, long> speeds;
     for(long i = 0; i < n; i++){
         long speed;
         std::cin >> speed;
