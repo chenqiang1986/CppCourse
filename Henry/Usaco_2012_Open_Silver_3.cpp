6a8b8eca7dc2a9be18eb3b416534c4707f8402eb
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2026-01-04 11:43:39 -0800

    Update Usaco_2012_Open_Silver_3.cpp

--- Henry/Usaco_2012_Open_Silver_3.cpp
+++ Henry/Usaco_2012_Open_Silver_3.cpp
@@ -5,11 +5,11 @@ long find_intersection_count(std::map<long, long>& speeds, long laps){
     std::map<long, long>::iterator it = std::next(speeds.end(), -1);
     long max_speed = it->first;
     long intersections = 0;
-    for(auto& [speed_one, count_one] : speeds){
-        for(auto& [speed_two, count_two] : speeds){
-            if(speed_one < speed_two){
-                long one_to_one = ((laps * (speed_two - speed_one)) / max_speed);
-                intersections += one_to_one * count_one * count_two;
+    for(std::map<long, long>::iterator i = speeds.begin(); i != speeds.end(); i++){
+        for(std::map<long, long>::iterator j = i; j != speeds.end(); j++){
+            if(i != j){
+                long one_to_one = ((laps * (j->first - i->first)) / max_speed);
+                intersections += one_to_one * j->second * i->second;
             }
         }
     }
