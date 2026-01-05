e7d7a5fc3756f827866a74e1847b815e27d22273
LegendSlayerX-YT <chenhenrybunny@gmail.com>
2026-01-04 17:47:59 -0800

    Create Usaco_2012_Open_Silver_3.cpp (#60)
    * Create Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp
    
    * Update Usaco_2012_Open_Silver_3.cpp

---
+++ Henry/Usaco_2012_Open_Silver_3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+
+long find_intersection_count(const std::map<long, long>& speeds, long laps){
+    std::map<long, long>::const_iterator it = std::next(speeds.end(), -1);
+    long max_speed = it->first;
+    long intersections = 0;
+    for(std::map<long, long>::const_iterator i = speeds.begin(); i != speeds.end(); i++){
+        for(std::map<long, long>::const_iterator j = i; j != speeds.end(); j++){
+            if(i != j){
+                long one_to_one = ((laps * (j->first - i->first)) / max_speed);
+                intersections += one_to_one * j->second * i->second;
+            }
+        }
+    }
+    return intersections;
+}
+
+int main(){
+    long n, l, c;
+    std::cin >> n >> l >> c;
+    std::map<long, long> speeds;
+    for(long i = 0; i < n; i++){
+        long speed;
+        std::cin >> speed;
+        speeds[speed]++;
+    }
+    std::cout << find_intersection_count(speeds, l) << std::endl;
+    return 0;
+}
