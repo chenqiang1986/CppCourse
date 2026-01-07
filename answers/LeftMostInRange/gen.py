5577ab7451472ea248f9623362179b40313b244a
chenqiang1986 <chenqiang19860101@gmail.com>
2026-01-06 19:37:49 -0800

    Update gen.py

--- answers/LeftMostInRange/gen.py
+++ answers/LeftMostInRange/gen.py
@@ -7,8 +7,12 @@ print(n)
 
 start_range = int(input())
 length_range = int(input())
+y_range = int(input())
 
 for i in range(n):
+    x = random.randint(-10000000, 10000000)
+    y = random.randint(-y_range, y_range)
+    print("Insert", x, y)
     start = random.randint(1, start_range)
     end = start + random.randint(0, length_range)
-    print(start, end)
+    print("Query", start, end)
